3ee46d3ef2f0a511331979729524bcad5afc190a
edim24 <edim24@gmail.com>
2017-10-05 17:44:53 +0300

    Frealtime image

---
+++ frealtime/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Future Realtime',
+    version='1.0',
+    long_description=__doc__,
+    packages=['frealtime'],
+    include_package_data=True,
+    zip_safe=False
+)
