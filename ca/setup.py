ad84154a67a65d5be8e7c4ccaab21ed94be18224
edim24 <edim24@gmail.com>
2018-04-05 12:57:12 +0300

    Content analysis python image

---
+++ ca/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='gensim image',
+    version='1.0',
+    long_description=__doc__,
+    packages=['gensim'],
+    include_package_data=True,
+    zip_safe=False
+)
